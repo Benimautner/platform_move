7223d1443113a8534c6ad92b453a62082504e86e
Benimautner <benimautner@gmail.com>
2022-01-24 20:45:26 +0100

    added comments

--- platform_move.ino
+++ platform_move.ino
@@ -1,12 +1,15 @@
 /*
   Author: Benedikt Mautner
   Date: 19.1.22
-  Descripton: This sketch acts as a controller for stepper motors.
+  Descripton: This sketch acts as a controller for stepper motors with the JSS57P2N controller.
   One short press of the buttons moves the platform to the stops.
   Long presses move the platform until the button is released. Endstops are always respected!
 
   Enable, Pulse and Direction pins can be the same for all steppers since they have to move the same distance anyways
   This could be changed if one wants different pulses/rev for each stepper, but I see no need for this
+
+  Buttons use input_pullup, so they should be connected to short to ground when pressed!
+
 */
 
 #define TYPE_TIME unsigned long
@@ -68,6 +71,7 @@ struct Controller {
   }
 
   void update_input_button(bool read, Button& btn) {
+    // if button is released, set status and prev_duration
     if (read) {
       btn.status = RELEASED;
       if (btn.duration > 0) {
@@ -75,6 +79,7 @@ struct Controller {
         btn.duration = 0;
       }
     } else {
+      // if button is pressed, record current time and set status
       if (btn.status == RELEASED) {
         btn.duration = millis();
       }
@@ -84,12 +89,13 @@ struct Controller {
 
   void update_buttons() {
 
+    // set variables for each available button
     update_input_button(digitalRead(UP_PIN), buttons.up);
     update_input_button(digitalRead(DOWN_PIN), buttons.down);
     update_input_button(digitalRead(TOP_LIMITER_PIN), buttons.top_limiter);
     update_input_button(digitalRead(BOTTOM_LIMITER_PIN), buttons.bottom_limiter);
 
-
+    // if  all buttons are released, disable button ignore
     if (buttons.up.status == RELEASED && buttons.down.status == RELEASED && buttons.ignore) {
       buttons.ignore = false;
       Serial.println("Ignore is off");
@@ -102,6 +108,7 @@ struct Controller {
   void update() {
     update_buttons();
 
+    // if one of the limiter is pressed, ignore buttons and drive in the other direction
     if (buttons.top_limiter.status || buttons.bottom_limiter.status) {
       buttons.ignore = true;
       digitalWrite(ENABLE_PIN, LOW);
@@ -114,6 +121,7 @@ struct Controller {
       driveStatus = STOPPED;
     }
 
+    //if opposite button of automatic direction is pressed, or both buttons are pressed, stop
     if ( (driveStatus == UP_TILL_STOP && buttons.down.status == PRESSED) ||
          (driveStatus == DOWN_TILL_STOP && buttons.up.status == PRESSED) ||
          (buttons.down.status == PRESSED && buttons.up.status == PRESSED)) {
@@ -121,26 +129,31 @@ struct Controller {
       buttons.ignore = true;
       Serial.println("Ignore is on");
     }
+    //dont change status if buttons are supposed to be ignored
     if (buttons.ignore) {
       return;
     }
 
+    // if button is pressed and the time pressed is longer than the specified short_press threshold, set to manual
     if (buttons.up.duration > 0 && millis() - buttons.up.duration > (TYPE_TIME)SHORT_PRESS_MS) {
       driveStatus = UP_MANUAL;
     }
     else if (buttons.down.duration > 0 && millis() - buttons.down.duration > (TYPE_TIME)SHORT_PRESS_MS) {
       driveStatus = DOWN_MANUAL;
     }
+    // if button is released and the time pressed is shorter than short_press threshold, set mode to automatic
     else if (buttons.up.prev_duration > 0 && buttons.up.prev_duration <= (TYPE_TIME)SHORT_PRESS_MS) {
       driveStatus = UP_TILL_STOP;
     }
     else if (buttons.down.prev_duration > 0 && buttons.down.prev_duration <= (TYPE_TIME)SHORT_PRESS_MS) {
       driveStatus = DOWN_TILL_STOP;
     }
+
+    // resetting durations after having dealt with them
     buttons.up.prev_duration = 0;
     buttons.down.prev_duration = 0;
 
-
+    // stop driving if manual mode is selected and button is released
     if ((driveStatus == UP_MANUAL && buttons.up.status == RELEASED) ||
         (driveStatus == DOWN_MANUAL && buttons.down.status == RELEASED)) {
       driveStatus = STOPPED;
@@ -153,6 +166,7 @@ struct Controller {
       digitalWrite(ENABLE_PIN, HIGH);
       return;
     }
+  
     if (driveStatus == UP_MANUAL || driveStatus == UP_TILL_STOP) {
       digitalWrite(ENABLE_PIN, LOW);
       digitalWrite(DIRECTION_PIN, DIRECTION_UP);
@@ -180,6 +194,7 @@ Controller controller;
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Code available at: https://github.com/Benimautner/platform_move");
 
   pinMode(PULSE_PIN, OUTPUT);
   pinMode(ENABLE_PIN, OUTPUT);
