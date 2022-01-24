489c7932528773abee891001a74c47962541d450
Benimautner <benimautner@gmail.com>
2022-01-24 20:34:15 +0100

    initial commit

---
+++ platform_move.ino
@@ -0,0 +1,212 @@
+/*
+  Author: Benedikt Mautner
+  Date: 19.1.22
+  Descripton: This sketch acts as a controller for stepper motors.
+  One short press of the buttons moves the platform to the stops.
+  Long presses move the platform until the button is released. Endstops are always respected!
+
+  Enable, Pulse and Direction pins can be the same for all steppers since they have to move the same distance anyways
+  This could be changed if one wants different pulses/rev for each stepper, but I see no need for this
+*/
+
+#define TYPE_TIME unsigned long
+
+#define UP_PIN 3
+#define DOWN_PIN 2
+#define PULSE_PIN 5
+#define ENABLE_PIN 6
+#define DIRECTION_PIN 4
+#define TOP_LIMITER_PIN 9
+#define BOTTOM_LIMITER_PIN 8
+
+#define PULSESPERREV 5000
+#define PULSE_DELAY_US 100 // can be as low as 5, according to datasheet of motor
+#define SHORT_PRESS_MS 200 // clicks of buttons under 200 ms count as short clicks
+
+#define DIRECTION_UP 0  // HIGH or LOW, depending on the preferred direction for upwards motion
+#define DIRECTION_DOWN !DIRECTION_UP
+
+enum ButtonStatus : int {
+  RELEASED,     // do not change order! RELEASED has to be 0 and pressed has to be 1
+  PRESSED
+};
+
+enum DriveStatus : int {
+  STOPPED,
+  UP_MANUAL,
+  DOWN_MANUAL,
+  UP_TILL_STOP,
+  DOWN_TILL_STOP,
+};
+
+struct Button {
+  TYPE_TIME duration = 0;
+  TYPE_TIME prev_duration = 0;
+  ButtonStatus status = RELEASED;
+};
+
+struct Buttons {
+  Button up;
+  Button down;
+  Button top_limiter;
+  Button bottom_limiter;
+
+  bool ignore = false;
+};
+
+struct Controller {
+  Buttons buttons;
+  int direction = -1;
+  int pulsesperrev = PULSESPERREV;
+  DriveStatus driveStatus = STOPPED;
+
+  void pulse() {
+    digitalWrite(PULSE_PIN, HIGH);
+    delayMicroseconds(PULSE_DELAY_US);
+    digitalWrite(PULSE_PIN, LOW);
+    delayMicroseconds(PULSE_DELAY_US);
+  }
+
+  void update_input_button(bool read, Button& btn) {
+    if (read) {
+      btn.status = RELEASED;
+      if (btn.duration > 0) {
+        btn.prev_duration = millis() - btn.duration;
+        btn.duration = 0;
+      }
+    } else {
+      if (btn.status == RELEASED) {
+        btn.duration = millis();
+      }
+      btn.status = PRESSED;
+    }
+  }
+
+  void update_buttons() {
+
+    update_input_button(digitalRead(UP_PIN), buttons.up);
+    update_input_button(digitalRead(DOWN_PIN), buttons.down);
+    update_input_button(digitalRead(TOP_LIMITER_PIN), buttons.top_limiter);
+    update_input_button(digitalRead(BOTTOM_LIMITER_PIN), buttons.bottom_limiter);
+
+
+    if (buttons.up.status == RELEASED && buttons.down.status == RELEASED && buttons.ignore) {
+      buttons.ignore = false;
+      Serial.println("Ignore is off");
+      buttons.up.prev_duration = 0;
+      buttons.down.prev_duration = 0;
+    }
+
+  }
+
+  void update() {
+    update_buttons();
+
+    if (buttons.top_limiter.status || buttons.bottom_limiter.status) {
+      buttons.ignore = true;
+      digitalWrite(ENABLE_PIN, LOW);
+      while (buttons.top_limiter.status || buttons.bottom_limiter.status) {
+        update_buttons();
+        digitalWrite(DIRECTION_PIN, buttons.top_limiter.status ? DIRECTION_DOWN : DIRECTION_UP);
+        pulse();
+      }
+      digitalWrite(ENABLE_PIN, HIGH);
+      driveStatus = STOPPED;
+    }
+
+    if ( (driveStatus == UP_TILL_STOP && buttons.down.status == PRESSED) ||
+         (driveStatus == DOWN_TILL_STOP && buttons.up.status == PRESSED) ||
+         (buttons.down.status == PRESSED && buttons.up.status == PRESSED)) {
+      driveStatus = STOPPED;
+      buttons.ignore = true;
+      Serial.println("Ignore is on");
+    }
+    if (buttons.ignore) {
+      return;
+    }
+
+    if (buttons.up.duration > 0 && millis() - buttons.up.duration > (TYPE_TIME)SHORT_PRESS_MS) {
+      driveStatus = UP_MANUAL;
+    }
+    else if (buttons.down.duration > 0 && millis() - buttons.down.duration > (TYPE_TIME)SHORT_PRESS_MS) {
+      driveStatus = DOWN_MANUAL;
+    }
+    else if (buttons.up.prev_duration > 0 && buttons.up.prev_duration <= (TYPE_TIME)SHORT_PRESS_MS) {
+      driveStatus = UP_TILL_STOP;
+    }
+    else if (buttons.down.prev_duration > 0 && buttons.down.prev_duration <= (TYPE_TIME)SHORT_PRESS_MS) {
+      driveStatus = DOWN_TILL_STOP;
+    }
+    buttons.up.prev_duration = 0;
+    buttons.down.prev_duration = 0;
+
+
+    if ((driveStatus == UP_MANUAL && buttons.up.status == RELEASED) ||
+        (driveStatus == DOWN_MANUAL && buttons.down.status == RELEASED)) {
+      driveStatus = STOPPED;
+    }
+
+  }
+
+  void drive() {
+    if (driveStatus == STOPPED) {
+      digitalWrite(ENABLE_PIN, HIGH);
+      return;
+    }
+    if (driveStatus == UP_MANUAL || driveStatus == UP_TILL_STOP) {
+      digitalWrite(ENABLE_PIN, LOW);
+      digitalWrite(DIRECTION_PIN, DIRECTION_UP);
+      // always do a quarter rev at once
+      for (int i = 0; i < 20; i++) {
+        pulse();
+      }
+    }
+    else if (driveStatus == DOWN_MANUAL || driveStatus == DOWN_TILL_STOP) {
+      digitalWrite(ENABLE_PIN, LOW);
+      digitalWrite(DIRECTION_PIN, DIRECTION_DOWN);
+      // we want a response time of < 20 ms.
+      // we spend 1 ms per pulse (at 100us width)
+      // so we cannot do more than 20 pulses at once
+      for (int i = 0; i < 100; i++) {
+        pulse();
+      }
+    }
+
+  }
+
+};
+
+Controller controller;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(PULSE_PIN, OUTPUT);
+  pinMode(ENABLE_PIN, OUTPUT);
+  pinMode(DIRECTION_PIN, OUTPUT);
+
+  pinMode(UP_PIN, INPUT_PULLUP);
+  pinMode(DOWN_PIN, INPUT_PULLUP);
+  pinMode(TOP_LIMITER_PIN, INPUT_PULLUP);
+  pinMode(BOTTOM_LIMITER_PIN, INPUT_PULLUP);
+
+  digitalWrite(ENABLE_PIN, HIGH);
+}
+
+void loop() {
+  controller.update();
+  controller.drive();
+
+  /*
+    Serial.println("Drive status: " + String(controller.driveStatus));
+    Serial.println("Ignore: " + String(controller.buttons.ignore));
+    Serial.println("Btn up: " + String(controller.buttons.up));
+    Serial.println("Btn down: " + String(controller.buttons.down));
+    Serial.println("Btn up duration: " + String(controller.buttons.up_duration));
+    Serial.println("Btn down duration: " + String(controller.buttons.down_duration));
+    Serial.println("Btn up prev: " + String(controller.buttons.up_prev_duration));
+    Serial.println("Btn down prev: " + String(controller.buttons.down_prev_duration));
+    Serial.flush();
+  */
+
+}
